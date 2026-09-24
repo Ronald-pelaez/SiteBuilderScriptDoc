fc3e6267a37aab4683fd2ae0a42706429a4c4bbc
Ronald Jimenez <ronaldpelaez201@gmail.com>
2026-09-24 17:22:11 -0500

    inicial

--- src/UserCard.vue
+++ src/UserCard.vue
@@ -27,10 +27,8 @@ interface User {
  */
 type UserStatus = 'active' | 'inactive' | 'pending';
 
-/**
- *
- * Defines the data received by the component.
- */
+
+
 const props = defineProps<{
   /**
    * User information.
